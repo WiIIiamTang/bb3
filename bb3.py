106de3bc87d663aff90e3f16bb4cf7322d4a1000
William Tang <48343678+WiIIiamTang@users.noreply.github.com>
2023-06-11 11:17:56 -0400

    normalize token env variable

--- bb3.py
+++ bb3.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 from src.bot.bb3_bot import main
 from src.util.constants import (
-    BB3_DEV_TOKEN,
+    BB3_TOKEN,
     BB3_PREFIX,
 )
 
 load_dotenv()
 
-asyncio.run(main(token=os.getenv(BB3_DEV_TOKEN), prefix=os.getenv(BB3_PREFIX)))
+asyncio.run(main(token=os.getenv(BB3_TOKEN), prefix=os.getenv(BB3_PREFIX)))
