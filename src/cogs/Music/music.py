01dd8965bd4ecd06c146f132a66fe3166702bd60
William Tang <48343678+WiIIiamTang@users.noreply.github.com>
2023-06-11 20:45:55 -0400

    add music radio

--- src/cogs/Music/music.py
+++ src/cogs/Music/music.py
@@ -1,12 +1,20 @@
 import discord
+import youtube_related
+import random
+import logging
+import asyncio
 from discord.ext import commands
 
 from src.cogs.Music.ytdl import YTDLSource
+from src.util.constants import BB3_MAX_RADIO_PLAY_COUNT
+
+logger = logging.getLogger(__name__)
 
 
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.current_url_radio = None  # TODO: Temporary
 
     @commands.command()
     async def join(self, ctx, *, channel: discord.VoiceChannel):
@@ -52,6 +60,55 @@ class Music(commands.Cog):
 
         await ctx.send(f"Now playing: {player.title}")
 
+    async def play_next(self, ctx, url, count):
+        if count <= 0:
+            return
+
+        related = await youtube_related.async_fetch(url)
+        related_id = random.choice(related).get("id")
+        url = f"https://www.youtube.com/watch?v={related_id}"
+
+        logger.info(f"Playing next song for {ctx.author.name}")
+        player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+        self.current_url_radio = url
+        ctx.voice_client.play(
+            player,
+            after=lambda e: asyncio.run_coroutine_threadsafe(
+                self.play_next(ctx, url, count - 1), self.bot.loop
+            ),
+        )
+        await ctx.send(f"Now playing: {player.title} {url}")
+
+    @commands.command(aliases=["nr"])
+    async def next_radio(self, ctx):
+        await self.stop(ctx)
+        if self.current_url_radio is None:
+            await ctx.send("No radio is playing")
+            return
+        await self.radio(ctx, url=self.current_url_radio)
+
+    @commands.command(aliases=["r"])
+    async def radio(self, ctx, *, url, count=BB3_MAX_RADIO_PLAY_COUNT):
+        """Streams from a url and plays a related song after"""
+
+        player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+        self.current_url_radio = url
+        ctx.voice_client.play(
+            player,
+            after=lambda e: asyncio.run_coroutine_threadsafe(
+                self.play_next(ctx, url, count - 1), self.bot.loop
+            ),
+        )
+        await ctx.send(f"Now playing: {player.title}")
+
+    @commands.command()
+    async def send_related(self, ctx, *, url):
+        """Sends a related song from a url"""
+
+        related = await youtube_related.async_fetch(url)
+        related_id = random.choice(related).get("id")
+        await ctx.send(f"https://www.youtube.com/watch?v={related_id}")
+
     @commands.command()
     async def volume(self, ctx, volume: int):
         """Changes the player's volume"""
@@ -71,6 +128,7 @@ class Music(commands.Cog):
     @play.before_invoke
     @dplay.before_invoke
     @play_local.before_invoke
+    @radio.before_invoke
     async def ensure_voice(self, ctx):
         if ctx.voice_client is None:
             if ctx.author.voice:
